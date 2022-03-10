e99d75885fed2ebd4121746b42c22ec24d100765
Andrei Calazans <andreixoc@hotmail.com>
2022-03-09 23:00:41 -0300

    Testing contract - multiple users can connect

--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,23 @@
 
 
 async function main() {
+  const [owner, somebodyElse] = await hre.ethers.getSigners();
   const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
   const keyboardsContract = await keyboardsContractFactory.deploy();
   await keyboardsContract.deployed();
   console.log("Contract deployed to:", keyboardsContract.address);
-  let keyboards = await keyboardsContract.getKeyboards();
-  console.log("We got the keyboards!", keyboards);
 
-  const keyboardTxn = await keyboardsContract.create("A really great keyboard!");
-  await keyboardTxn.wait();
+  const keyboardTxn1 = await keyboardsContract.create("A really great keyboard!");
+  await keyboardTxn1.wait();
+
+  const keyboardTx2 = await keyboardsContract.connect(somebodyElse).create("An even better keyboard!");
+  await keyboardTx2.wait();
 
   keyboards = await keyboardsContract.getKeyboards();
   console.log("We got the keyboards!", keyboards);
+
+  keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+  console.log("And as somebody else!", keyboards);
 }
 
 
