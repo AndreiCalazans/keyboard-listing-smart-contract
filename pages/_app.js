6c89cfd995e75ebceffceb8889b5d4af0f28913c
Andrei Calazans <andreixoc@hotmail.com>
2022-03-10 23:29:11 -0300

    Include check for expected chain

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "../styles/globals.css";
+import { useEffect } from 'react';
 import { Toaster } from "react-hot-toast";
 
+const checkEthereumChain = async () => {
+  if (window.ethereum) {
+    // Reload if chain changes, see <https://docs.metamask.io/guide/ethereum-provider.html#chainchanged>
+    window.ethereum.on("chainChanged", (_chainId) => window.location.reload());
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    const rinkebyId = "0x4"; // See <https://docs.metamask.io/guide/ethereum-provider.html#chain-ids>
+    if (chainId !== rinkebyId) {
+      alert("Please use Rinkeby network");
+      setEthereum(window.ethereum);
+    }
+  }
+};
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => checkEthereumChain, [])
   return (
     <>
       <Toaster />
